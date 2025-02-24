658f62e3452dbf9ea8319100eaf767e602ed4d82
Tobias Wawak <tobiaswawak@online.de>
2025-02-24 21:58:51 +0100

    login and register

--- firebase.js
+++ firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByAY9UaX54HDpXY5_Kx7C1LuU2jKf4tbw",
-    authDomain: "on23-colourpicker-693f6.firebaseapp.com",
-    projectId: "on23-colourpicker-693f6",
-    storageBucket: "on23-colourpicker-693f6.firebasestorage.app",
-    messagingSenderId: "333660382782",
-    appId: "1:333660382782:web:e86370e235aae6903f936f"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 
