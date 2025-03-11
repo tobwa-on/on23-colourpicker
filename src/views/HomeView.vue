77d1bc57f4a5b0c1cf712ca90a767b75aad9bedc
Tobias Wawak <tobiaswawak@online.de>
2025-03-11 14:37:43 +0100

    rename home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@
 </template>
 
 <script setup>
-import Home from "../components/Home2.vue";
+import Home from "../components/Home.vue";
 import BottomNavbar from "../components/BottomNavbar.vue";
-import Home2 from "../components/Home2.vue";
+import Home2 from "../components/Home.vue";
 
 </script>
 
