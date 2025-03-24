e3581ecbbeaf572aa7382b0b1d1d580745a11b16
Jonas Bauer <jonas.bauer90@outlook.de>
2025-03-24 13:16:43 +0100

    added: auto registration of service worker

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,44 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'; // Importiere das Vue-Plugin
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    strategies: 'injectManifest',
-    srcDir: 'src',
-    filename: 'sw.js',
-    registerType: 'prompt',
-    injectRegister: false,
-
-    manifest: {
-      name: 'on23-colourpicker',
-      short_name: 'on23-colourpicker',
-      description: 'on23-colourpicker',
-      theme_color: '#ffffff',
-
-      icons: [{
-        src: 'pwa-64x64.png',
-        sizes: '64x64',
-        type: 'image/png',
-      }, {
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      }, {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      }, {
-        src: 'maskable-icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'maskable',
-      }],
-    },
-
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-    },
-
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+  plugins: [
+    vue(), // Vue-Plugin hinzufügen
+    VitePWA({
+      registerType: 'autoUpdate',  // Service Worker wird automatisch aktualisiert
+      devOptions: {
+        enabled: true,  // Ermöglicht den Service Worker im Entwicklungsmodus
+      },
+      manifest: {
+        name: 'on23-colourpicker',
+        short_name: 'on23-colourpicker',
+        description: 'on23-colourpicker',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+});
